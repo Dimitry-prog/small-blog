96a2adfa26e78ed75118fa5fd434f659ae8fea4d
Dimitry-prog <kfifa3@gmail.com>
2023-04-03 18:23:53 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="my-12 text-3xl text-center dark:text-white"> Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">I'm <span className="font-bold">Dimitry</span> </span>.
+      </p>
+    </main>
+  )
+}
